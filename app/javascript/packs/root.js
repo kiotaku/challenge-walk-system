6cdcf4e1d73aae0bf8a16b00dad62082472d558a
kiotaku <takumi.kioka@gmail.com>
2018-11-06 23:02:12 +0900

    fix bugs

--- app/javascript/packs/root.js
+++ app/javascript/packs/root.js
@@ -3,6 +3,6 @@ import ReactDom from 'react-dom'
 import axios from 'axios'
 import Root from 'Root'
 
-axios.defaults.headers['X-CSRF-TOKEN'] = document.querySelectorAll('meta[name=csrf-token]')[0].getAttribute('content')
+axios.defaults.headers['X-CSRF-Token'] = document.querySelectorAll('meta[name=csrf-token]')[0].getAttribute('content')
 
 ReactDom.render(<Root/>, document.getElementById('app'))
